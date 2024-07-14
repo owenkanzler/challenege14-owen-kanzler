167b79708994221c0a3938ca09fa60535347b94d
Owen Kanzler <oj.kanz1@gmail.com>
2024-07-13 19:19:51 -0500

    set everything up and finished the models

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleString(),
+  },
+});
+
+module.exports = reactionSchema;
